ed1ca1df56ee0d4a659931a636e529ba75b60394
rifaielarbi <153360442+rifaielarbi@users.noreply.github.com>
2023-12-16 17:10:11 +0100

    first app

---
+++ lib/Screens/Page3.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../Components/menu.dart';
+
+class Page3 extends StatefulWidget {
+  const Page3({Key? key}) : super(key: key);
+  @override
+  State<Page3> createState() => _Page3State();
+}
+
+class _Page3State extends State<Page3> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Menu(),
+      appBar: AppBar(
+        title: Text("Page1"),
+        backgroundColor: Color.fromRGBO(36, 92, 186, 1.0),
+      ),
+      body: Center(
+        child: CircleAvatar(
+          backgroundImage: AssetImage("assets/images/3.jpg"),
+          radius: 80,
+        ),
+      ),
+    );
+  }
+}
